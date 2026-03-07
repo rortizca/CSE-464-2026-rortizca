b279b4a7948a6ede41e48259a6e5f4c48ae3121d
Dankymarsupial <dankymarsupial1969@gmail.com>
2026-03-07 13:50:56 -0700

    Adding residual files for feature 4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import guru.nidi.graphviz.parse.Parser;
 import guru.nidi.graphviz.model.MutableGraph;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.engine.Format;
+import static guru.nidi.graphviz.model.Factory.*;
 
 import java.io.IOException;
 import java.io.FileWriter;
@@ -14,6 +15,7 @@ public class Main {
 
     }
 
+    // FEATURE 1
     public static MutableGraph parseGraph(String filepath) throws Exception {
         try {
             return new Parser().read(new File(filepath));
@@ -25,7 +27,34 @@ public class Main {
     }
 
     public static String toString(MutableGraph graph) {
-       return graph.toString();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Number of nodes: ").append(graph.nodes().size()).append("\n");
+
+        sb.append("Nodes: \n");
+
+        for (MutableNode node : graph.nodes()) {
+            sb.append(node.name()).append("\n");
+        }
+
+        int edgeCount = 0;
+
+        sb.append("Edges: \n");
+
+        for (MutableNode node : graph.nodes()) {
+            node.links().forEach(link -> {
+                sb.append(node.name())
+                        .append(" -> ")
+                        .append(link.to().name())
+                        .append("\n");
+
+            });
+            edgeCount += node.links().size();
+        }
+
+        sb.append("Number of Edges: ").append(edgeCount).append("\n");
+
+        return sb.toString();
     }
 
     public static void outputGraph(MutableGraph g, String filepath) {
@@ -37,4 +66,88 @@ public class Main {
         }
 
     }
+
+    // FEATURE 2
+    public static void addNode(MutableGraph g, String label) {
+        for (MutableNode node : g.nodes()) {
+            if (node.name().toString().equals(label)) {
+                System.out.println("Duplicate Node Found: " + label);
+                return;
+            }
+
+        }
+
+        g.add(mutNode(label));
+    }
+
+    public static void addNodes(MutableGraph g, String[] labels) {
+
+        for (String label : labels) {
+            addNode(g, label);
+        }
+    }
+
+    // FEATURE 3
+    public static void addEdge(MutableGraph g, String srcLabel, String dstLabel) {
+        // IMPLEMENTED
+
+        MutableNode srcNode = null;
+        MutableNode dstNode = null;
+
+        for (MutableNode node : g.nodes()) {
+            if (node.name().toString().equals(srcLabel)) {
+                srcNode = node;
+            }
+
+            if (node.name().toString().equals(dstLabel)) {
+                dstNode = node;
+            }
+        }
+
+        if (srcNode == null || dstNode == null) {
+            System.out.println("Could not find source or destination node");
+            return;
+        }
+
+        for (var link : srcNode.links()) {
+            if (link.to().name().toString().equals(dstLabel)) {
+                System.out.println("Requested edge already exists: " + srcLabel + " -> " + dstLabel);
+                return;
+            }
+        }
+
+        srcNode.addLink(dstNode);
+    }
+
+    // FEATURE 4
+    public static void outputDotGraph(MutableGraph g, String path) {
+        // IMPLEMENTED
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write(g.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void outputGraphics(MutableGraph g, String path, String format) {
+        // Implemented
+        try {
+
+            Format fmt;
+
+            if (format.equalsIgnoreCase("png")) {
+                fmt = Format.PNG;
+            } else {
+                throw new IllegalArgumentException("Unsupported format");
+            }
+
+            Graphviz.fromGraph(g)
+                    .render(fmt)
+                    .toFile(new File(path));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
